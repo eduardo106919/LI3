02739bccfdb54ff0e7dfe7b605008c27841ce289
eduardo106919 <a106919@alunos.uminho.pt>
2024-09-25 21:56:01 +0100

    run code formatter

--- Guioes/Guiao3/examples/node.c
+++ Guioes/Guiao3/examples/node.c
@@ -3,8 +3,8 @@
 #include "node.h"
 #include <stdlib.h>
 
-Node * nodeCreate(void *data) {
-    Node * new = malloc(sizeof(Node));
+Node *nodeCreate(void *data) {
+    Node *new = malloc(sizeof(Node));
     new->data = data;
     new->next = NULL;
     return new;
